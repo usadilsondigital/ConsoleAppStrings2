34ab0c30b5eddcd33bf5657d5dfe8a54a02ae986
Adilson <usadilsondigital@gmail.com>
2024-02-19 11:16:54 -0600

    tr culture

--- StringCompare.cs
+++ StringCompare.cs
@@ -133,6 +133,15 @@ namespace ConsoleAppStrings2
             Console.WriteLine($"   Does 'Bill' start with 'BILL'? {name.StartsWith("BILL", true, null)}");
             Console.WriteLine();
 
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("tr-TR");
+            Console.WriteLine($"Culture = {Thread.CurrentThread.CurrentCulture.DisplayName}");
+            Console.WriteLine($"   Is 'Bill' the same as 'BILL'? {name.Equals("BILL", StringComparison.OrdinalIgnoreCase)}");
+            Console.WriteLine($"   Does 'Bill' start with 'BILL'? {name.StartsWith("BILL", true, null)}");
+
+
+
+
         }
 
 
