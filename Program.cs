3b2a2dd1f12895abd8bebe254834ecb917663b20
Adilson <usadilsondigital@gmail.com>
2024-02-18 20:36:29 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppStrings2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
