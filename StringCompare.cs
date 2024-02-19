92f72283c1531c212040d30b1b41b0a297bb39d6
Adilson <usadilsondigital@gmail.com>
2024-02-18 20:58:41 -0600

    console the results

--- StringCompare.cs
+++ StringCompare.cs
@@ -71,7 +71,10 @@ namespace ConsoleAppStrings2
             Console.WriteLine();
             Console.WriteLine("str1 = '{0}', str2 = '{1}'", str1, str2);
             result = String.Compare(str1, 2, str2, 0, 2);
-
+            str = ((result < 0) ? "less than" : ((result > 0) ? "greater than" : "equal to"));
+            Console.Write("Substring '{0}' in '{1}' is ", str1.Substring(2, 2), str1);
+            Console.Write("{0} ", str);
+            Console.WriteLine("substring '{0}' in '{1}'.", str2.Substring(0, 2), str2);
 
         }
 
