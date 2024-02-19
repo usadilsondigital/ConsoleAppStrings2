05248b4476ebe8e846058f89067bf08ea76a4788
Adilson <usadilsondigital@gmail.com>
2024-02-19 11:41:09 -0600

    define 4 letter words

--- StringConcat.cs
+++ StringConcat.cs
@@ -10,5 +10,12 @@ namespace ConsoleAppStrings2
     //Concatenates one or more instances of String, or the String representations of the values of one or more instances of Object
     internal class StringConcat
     {
+        const int WORD_SIZE = 4;
+
+        public void ScrambleGame() {
+            // Define some 4-letter words to be scrambled.
+            string[] words = { "home", "food", "game", "rest" };
+
+        }
     }
 }
