1315e10dfe90d625e1025f559cf7acde55d24be8
Adilson <usadilsondigital@gmail.com>
2024-02-19 22:34:44 -0600

    concat 5 return docs

--- StringConcat.cs
+++ StringConcat.cs
@@ -59,10 +59,22 @@ namespace ConsoleAppStrings2
             Console.WriteLine("5) {0}", String.Concat(o, o, o, o, o));
             Console.WriteLine("\nConcatenate a 3-element object array:");
             Console.WriteLine("6) {0}", String.Concat(objs));
+            // The example displays the following output:
+            //    Concatenate 1, 2, and 3 objects:
+            //    1) -123
+            //    2) -123-123
+            //    3) -123-123-123
+            //
+            //    Concatenate 4 objects and a variable length parameter list:
+            //    4) -123-123-123-123
+            //    5) -123-123-123-123-123
+            //
+            //    Concatenate a 3-element object array:
+            //    6) -123-456-789
         }
 
 
 
 
-        }
+    }
     }
