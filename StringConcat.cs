92fd906badd487a96bc3a43d108ac2cc583a0d9b
Adilson <usadilsondigital@gmail.com>
2024-02-20 12:17:36 -0600

    mname

--- StringConcat.cs
+++ StringConcat.cs
@@ -79,6 +79,11 @@ namespace ConsoleAppStrings2
             string fName = "Simon";
             string mName = "Jake";
             string lName = "Harrows";
+            // because we want a name to appear with a space in between each name,
+            // put a space on the front of the middle, and last name, allowing for
+            // the fact that a space may already be there
+            mName = " " + mName.Trim();
+            lName = " " + lName.Trim();
         }
 
 
