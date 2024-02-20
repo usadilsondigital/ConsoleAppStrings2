7785bcb6dc70a60621e2af36683d7f7f87c48293
Adilson <usadilsondigital@gmail.com>
2024-02-20 12:53:54 -0600

    ordinal contains method

--- StringContains.cs
+++ StringContains.cs
@@ -35,6 +35,19 @@ namespace ConsoleAppStrings2
             return str.IndexOf(substring, comp) >= 0;
         }
 
+        public void OrdinalContains() {
+
+            String s = "This is a string.";
+            String sub1 = "this";
+            Console.WriteLine("Does '{0}' contain '{1}'?", s, sub1);
+            StringComparison comp = StringComparison.Ordinal;
+            Console.WriteLine("   {0:G}: {1}", comp, s.Contains(sub1, comp));
+
+            comp = StringComparison.OrdinalIgnoreCase;
+            Console.WriteLine("   {0:G}: {1}", comp, s.Contains(sub1, comp));
+
+        }
+
 
     }
 }
