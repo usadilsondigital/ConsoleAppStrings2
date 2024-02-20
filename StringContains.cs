aaa1aa7d1481dfda041e18abf1374db8136c8cfb
Adilson <usadilsondigital@gmail.com>
2024-02-20 12:46:10 -0600

    example output

--- StringContains.cs
+++ StringContains.cs
@@ -20,6 +20,9 @@ namespace ConsoleAppStrings2
                 if (index >= 0)
                     Console.WriteLine("'{0} begins at character position {1}",s2, index + 1);
             }
+            // This example displays the following output:
+            //    'fox' is in the string 'The quick brown fox jumps over the lazy dog': True
+            //    'fox begins at character position 17
         }
 
 
