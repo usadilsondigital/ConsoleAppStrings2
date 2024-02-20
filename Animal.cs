08f2a26abe23a157436c06ca6197267b6f234d9d
Adilson <usadilsondigital@gmail.com>
2024-02-20 12:34:03 -0600

    class animal

---
+++ Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    public class Animal
+    {
+        public string Kind;
+        public string Order;
+
+        public Animal(string kind, string order)
+        {
+            this.Kind = kind;
+            this.Order = order;
+        }
+
+        public override string ToString()
+        {
+            return this.Kind;
+        }
+    }
+}
