f596bd1d0ae4bb5d27afd07bfabb62a9d0f42098
Adilson <usadilsondigital@gmail.com>
2024-02-20 23:23:37 -0600

    class  string copyto

---
+++ StringCopyTo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    internal class StringCopyTo
+    {
+
+        // Embed an array of characters in a string
+        string strSource = "changed";
+        char[] destination = { 'T', 'h', 'e', ' ', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ',
+                'a', 'r', 'r', 'a', 'y' };
+
+    }
+}
