f937112ae747f45a54569e5cfbb975d2ce0aad48
Adilson <usadilsondigital@gmail.com>
2024-02-20 23:24:46 -0600

    copy to test method

--- StringCopyTo.cs
+++ StringCopyTo.cs
@@ -14,5 +14,13 @@ namespace ConsoleAppStrings2
         char[] destination = { 'T', 'h', 'e', ' ', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ',
                 'a', 'r', 'r', 'a', 'y' };
 
+        public void CopyToTest() {
+
+            // Print the char array
+            Console.WriteLine(destination);
+            // Embed the source string in the destination string
+            strSource.CopyTo(0, destination, 4, strSource.Length);
+        }
+
     }
 }
