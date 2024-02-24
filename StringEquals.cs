0ce3b97bd42ab9ed545d805e7dec6cef60efb4da
Adilson <usadilsondigital@gmail.com>
2024-02-23 21:59:00 -0600

    string equals docs and class

---
+++ StringEquals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Determines whether this instance and a specified object, which must also be a String object, have the same value.
+    public class StringEquals
+    {
+
+    }
+}
