c95a91721d2600dbe409c69579ea96009bf6ff7b
Adilson <usadilsondigital@gmail.com>
2024-02-24 22:43:08 -0600

    temperature info

--- StringFormat.cs
+++ StringFormat.cs
@@ -58,13 +58,21 @@ namespace ConsoleAppStrings2
             Dictionary<DateTime, Double> temperatureInfo = new Dictionary<DateTime, Double>();
             temperatureInfo.Add(new DateTime(2010, 6, 1, 14, 0, 0), 87.46);
             temperatureInfo.Add(new DateTime(2010, 12, 1, 10, 0, 0), 36.81);
+            Console.WriteLine("Temperature Information:\n");
+            string output;
+            foreach (var item in temperatureInfo)
+            {
+                output = String.Format("Temperature at {0,8:t} on {0,9:d}: {1,5:N1}°F",
+                                       item.Key, item.Value);
+                Console.WriteLine(output);
+            }
+           
 
-            
 
         }
-    
-    
-    
-    
+
+
+
+
     }
     }
