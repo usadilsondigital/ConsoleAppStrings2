8fef60a1b87aa30adfd6c91a7cd11f166a4e9718
Adilson <usadilsondigital@gmail.com>
2024-02-24 22:32:22 -0600

    sting format class

---
+++ StringFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Converts the value of objects to strings based on the formats specified and inserts them into another string.
+    public class StringFormat
+    {
+
+        //Use String.Format if you need to insert the value of an object, variable, or expression into another string.
+        //For example, you can insert the value of a Decimal value into a string to display it to the user as a single string:
+
+    }
+}
