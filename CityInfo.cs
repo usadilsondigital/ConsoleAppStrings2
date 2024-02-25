d1d95e146890df7148b2f5b3b3ed326c7d7a64e6
Adilson <usadilsondigital@gmail.com>
2024-02-24 22:54:28 -0600

    city info class

---
+++ CityInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    public class CityInfo
+    {
+        public CityInfo(String name, int population, Decimal area, int year)
+        {
+            this.Name = name;
+            this.Population = population;
+            this.Area = area;
+            this.Year = year;
+        }
+
+        public readonly String Name;
+        public readonly int Population;
+        public readonly Decimal Area;
+        public readonly int Year;
+    }
+}
