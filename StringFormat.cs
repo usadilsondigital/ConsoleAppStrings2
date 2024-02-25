a5b9450ca87498f1397cabffa1d09ba3e4a25364
Adilson <usadilsondigital@gmail.com>
2024-02-24 22:52:39 -0600

    example output

--- StringFormat.cs
+++ StringFormat.cs
@@ -109,6 +109,13 @@ namespace ConsoleAppStrings2
             string result2 = String.Format("Temperature on {0:d}:\n{1,11}: {2} degrees (hi)\n{3,11}: {4} degrees (lo)",
                                new object[] { date1, hiTime, hiTemp, loTime, loTemp });
             Console.WriteLine(result2);
+            // The example displays output like the following:
+            //       Temperature on 7/1/2009:
+            //          14:17:32: 62.1 degrees (hi)
+            //          03:16:10: 54.8 degrees (lo)
+            //       Temperature on 7/1/2009:
+            //          14:17:32: 62.1 degrees (hi)
+            //          03:16:10: 54.8 degrees (lo)
 
         }
 
