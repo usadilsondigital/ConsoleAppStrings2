efaadac26a2b16451351bb460464e96089a64f39
Adilson <usadilsondigital@gmail.com>
2024-02-25 21:39:46 -0600

    enumerate n display

---
+++ StringGetEnumerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    public class StringGetEnumerator
+    {
+        public void EnumerateAndDisplay(String phrase)
+        {
+            Console.WriteLine("The characters in the string \"{0}\" are:",phrase);
+            int CharCount = 0;
+            int controlChars = 0;
+            int alphanumeric = 0;
+            int punctuation = 0;
+
+           
+        }
+    }
+
+
+}
