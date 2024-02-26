e4acea43ecbc5ddd8b4b9a2de32b696922fdbb43
Adilson <usadilsondigital@gmail.com>
2024-02-25 22:24:59 -0600

    example output

--- StringGetTypeCode.cs
+++ StringGetTypeCode.cs
@@ -14,6 +14,10 @@ namespace ConsoleAppStrings2
             String str = "abc";
             TypeCode tc = str.GetTypeCode();
             Console.WriteLine("The type code for '{0}' is {1}, which represents {2}.",str, tc.ToString("D"), tc.ToString("F"));
+            /*
+                This example produces the following results:
+                The type code for 'abc' is 18, which represents String.
+                */
         }
     }
 }
