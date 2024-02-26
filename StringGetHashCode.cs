4302cb1d490593fb8649b93c727edeb86a41d308
Adilson <usadilsondigital@gmail.com>
2024-02-25 22:17:16 -0600

    test case method

--- StringGetHashCode.cs
+++ StringGetHashCode.cs
@@ -13,5 +13,23 @@ namespace ConsoleAppStrings2
             int HashCode = Operand.GetHashCode();
             Console.WriteLine("The hash code for \"{0}\" is: 0x{1:X8}, {1}", Operand, HashCode);
         }
+
+        public  void TestCase()
+        {
+            DisplayHashCode("");
+            DisplayHashCode("a");
+            DisplayHashCode("ab");
+            DisplayHashCode("abc");
+            DisplayHashCode("abd");
+            DisplayHashCode("abe");
+            DisplayHashCode("abcdef");
+            DisplayHashCode("abcdeg");
+            DisplayHashCode("abcdeh");
+            DisplayHashCode("abcdei");
+            DisplayHashCode("Abcdeg");
+            DisplayHashCode("Abcdeh");
+            DisplayHashCode("Abcdei");
+        }
+
     }
 }
