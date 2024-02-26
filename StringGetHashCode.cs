afaf3725deec7d910df8477fb82fb5e0b86a74d3
Adilson <usadilsondigital@gmail.com>
2024-02-25 22:16:25 -0600

    display hash code method

--- StringGetHashCode.cs
+++ StringGetHashCode.cs
@@ -8,5 +8,10 @@ namespace ConsoleAppStrings2
 {
     public class StringGetHashCode
     {
+        public void DisplayHashCode(String Operand)
+        {
+            int HashCode = Operand.GetHashCode();
+            Console.WriteLine("The hash code for \"{0}\" is: 0x{1:X8}, {1}", Operand, HashCode);
+        }
     }
 }
