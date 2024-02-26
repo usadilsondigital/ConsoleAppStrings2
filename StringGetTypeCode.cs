6c10807d96c3f1a27538389228b28e82adaa3c4f
Adilson <usadilsondigital@gmail.com>
2024-02-25 22:24:30 -0600

    test case

--- StringGetTypeCode.cs
+++ StringGetTypeCode.cs
@@ -8,5 +8,12 @@ namespace ConsoleAppStrings2
 {
     public class StringGetTypeCode
     {
+
+        public static void TestCase()
+        {
+            String str = "abc";
+            TypeCode tc = str.GetTypeCode();
+            Console.WriteLine("The type code for '{0}' is {1}, which represents {2}.",str, tc.ToString("D"), tc.ToString("F"));
+        }
     }
 }
