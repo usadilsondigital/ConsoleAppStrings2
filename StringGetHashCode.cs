07120fa85c2856b4b54bdb365b5e98ce65d3e145
Adilson <usadilsondigital@gmail.com>
2024-02-25 22:19:44 -0600

    class docs

--- StringGetHashCode.cs
+++ StringGetHashCode.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppStrings2
 {
+    //Returns the hash code for this string.
     public class StringGetHashCode
     {
         public void DisplayHashCode(String Operand)
