9684e35fe0b0c9abc1cc088d0092599002a14453
Adilson <usadilsondigital@gmail.com>
2024-02-25 21:43:42 -0600

    example output

--- StringGetEnumerator.cs
+++ StringGetEnumerator.cs
@@ -36,6 +36,37 @@ namespace ConsoleAppStrings2
 
         }
 
+        // The example displays the following output:
+        //    The characters in the string "Test Case" are:
+        //    'T' 'e' 's' 't' ' ' 'C' 'a' 's' 'e'
+        //       Total characters:          9
+        //       Alphanumeric characters:   8
+        //       Punctuation characters:    0
+        //       Control Characters:        0
+        //    
+        //    The characters in the string "This is a sentence." are:
+        //    'T' 'h' 'i' 's' ' ' 'i' 's' ' ' 'a' ' ' 's' 'e' 'n' 't' 'e' 'n' 'c' 'e' '.'
+        //       Total characters:         19
+        //       Alphanumeric characters:  15
+        //       Punctuation characters:    1
+        //       Control Characters:        0
+        //    
+        //    The characters in the string "Has       two     tabs" are:
+        //    'H' 'a' 's' '0x0009' 't' 'w' 'o' '0x0009' 't' 'a' 'b' 's'
+        //       Total characters:         12
+        //       Alphanumeric characters:  10
+        //       Punctuation characters:    0
+        //       Control Characters:        2
+        //    
+        //    The characters in the string "Two
+        //    new
+        //    lines" are:
+        //    'T' 'w' 'o' '0x000A' 'n' 'e' 'w' '0x000A' 'l' 'i' 'n' 'e' 's'
+        //       Total characters:         13
+        //       Alphanumeric characters:  11
+        //       Punctuation characters:    0
+        //       Control Characters:        2
+
 
 
     }
