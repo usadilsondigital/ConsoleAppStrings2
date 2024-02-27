1ede32c0c3dc159d52293b496121906e08d0d315
Adilson <usadilsondigital@gmail.com>
2024-02-26 22:57:15 -0600

    index of char method

--- StringIndexOf.cs
+++ StringIndexOf.cs
@@ -82,5 +82,14 @@ namespace ConsoleAppStrings2
             //       -1
         }
 
+        public void IndexOfChar() {
+
+            string br1 = "0----+----1----+----2----+----3----+----4----+----5----+----6----+---";
+            string br2 = "012345678901234567890123456789012345678901234567890123456789012345678";
+            string str = "Now is the time for all good men to come to the aid of their country.";
+            int start;
+            int at;
+        }
+
     }
 }
