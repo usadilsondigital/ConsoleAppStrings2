704551a3e14a2b3ca8099eb2c8134c59c0e025dc
Adilson <usadilsondigital@gmail.com>
2024-02-26 22:48:52 -0600

    test case

--- StringIndexOf.cs
+++ StringIndexOf.cs
@@ -8,6 +8,10 @@ namespace ConsoleAppStrings2
 {
     public class StringIndexOf
     {
-
+        public void TestCase() {
+            string br1 = "0----+----1----+----2----+----3----+----4----+----5----+----6----+---";
+            string br2 = "012345678901234567890123456789012345678901234567890123456789012345678";
+            string str = "Now is the time for all good men to come to the aid of their country.";
+        }
     }
 }
