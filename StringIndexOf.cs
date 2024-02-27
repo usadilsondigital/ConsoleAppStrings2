1a8577e1f2712b4480b476bd4614efc0039c0843
Adilson <usadilsondigital@gmail.com>
2024-02-26 22:49:11 -0600

    init variables

--- StringIndexOf.cs
+++ StringIndexOf.cs
@@ -12,6 +12,10 @@ namespace ConsoleAppStrings2
             string br1 = "0----+----1----+----2----+----3----+----4----+----5----+----6----+---";
             string br2 = "012345678901234567890123456789012345678901234567890123456789012345678";
             string str = "Now is the time for all good men to come to the aid of their country.";
+            int start;
+            int at;
+            int end;
+            int count;
         }
     }
 }
