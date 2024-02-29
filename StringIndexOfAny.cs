a0ea4212883fc8b8aa9265ec055d2ea05352b9f0
Adilson <usadilsondigital@gmail.com>
2024-02-28 21:37:27 -0600

    string index of any docs

--- StringIndexOfAny.cs
+++ StringIndexOfAny.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppStrings2
 {
+    //Reports the index of the first occurrence in this instance of any character in a specified array of Unicode characters.
+    //The method returns -1 if the characters in the array are not found in this instance.
     public class StringIndexOfAny
     {
     }
