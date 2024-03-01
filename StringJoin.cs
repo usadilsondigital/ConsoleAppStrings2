18914649e458105185213413232af83007987e5d
Adilson <usadilsondigital@gmail.com>
2024-02-29 21:34:15 -0600

    test case

---
+++ StringJoin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Concatenates the elements of a specified
+    //array or the members of a collection, using the specified separator between each element or member.
+    public class StringJoin
+    {
+        public  void TestCase()
+        {
+            int maxPrime = 100;
+            List<int> primes = GetPrimes(maxPrime);
+            Console.WriteLine("Primes less than {0}:", maxPrime);
+            Console.WriteLine("   {0}", String.Join(" ", primes));
+        }
+
+    }
+}
