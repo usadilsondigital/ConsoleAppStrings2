0f913a9a56847e276ec2834124dc7663b30ae198
Adilson <usadilsondigital@gmail.com>
2024-02-29 21:37:25 -0600

    get primes docs

--- StringJoin.cs
+++ StringJoin.cs
@@ -39,9 +39,11 @@ namespace ConsoleAppStrings2
                     primes.Add(ctr);
             return primes;
         }
+        // The example displays the following output:
+        //    Primes less than 100:
+        //       2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
 
 
 
-
-        }
+    }
 }
