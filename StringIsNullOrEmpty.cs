499561e5b672603833f0251ee3f05ba28b86d601
Adilson <usadilsondigital@gmail.com>
2024-02-29 21:27:45 -0600

    console test example strings

--- StringIsNullOrEmpty.cs
+++ StringIsNullOrEmpty.cs
@@ -9,5 +9,16 @@ namespace ConsoleAppStrings2
     //Indicates whether the specified string is null or an empty string ("").
     public class StringIsNullOrEmpty
     {
+        public void TestCase() {
+            string s1 = "abcd";
+            string s2 = "";
+            string s3 = null;
+
+            Console.WriteLine("String s1 {0}.", Test(s1));
+            Console.WriteLine("String s2 {0}.", Test(s2));
+            Console.WriteLine("String s3 {0}.", Test(s3));
+
+            
+        }
     }
 }
