5bbdd76e60262a2063a2c92a9d554afed317fd2a
Adilson <usadilsondigital@gmail.com>
2024-03-01 21:22:58 -0600

    string last index of

---
+++ StringLastIndexOf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Reports the zero-based index position of the last occurrence of a specified Unicode character or string
+    //within this instance. The method returns -1 if the character or string is not found in this instance
+    public class StringLastIndexOf
+    {
+    }
+}
