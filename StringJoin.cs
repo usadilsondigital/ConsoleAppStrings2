1243b5069dd9934fd323f1b34a05eb29f9361afc
Adilson <usadilsondigital@gmail.com>
2024-03-01 21:09:58 -0600

    string join sep and console

--- StringJoin.cs
+++ StringJoin.cs
@@ -94,6 +94,9 @@ namespace ConsoleAppStrings2
             String result;
 
             Console.WriteLine("sep = '{0}'", sep);
+            Console.WriteLine("val[] = {{'{0}' '{1}' '{2}' '{3}'}}", val[0], val[1], val[2], val[3]);
+            result = String.Join(sep, val, 1, 2);
+            Console.WriteLine("String.Join(sep, val, 1, 2) = '{0}'", result);
         }
 
 
