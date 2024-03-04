e5e0ffa4bd56524dea8fb84e2cced3726f7fab93
Adilson <usadilsondigital@gmail.com>
2024-03-03 23:11:59 -0600

    class docs

--- StringPadRight.cs
+++ StringPadRight.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppStrings2
 {
+    //Returns a new string of a specified length in which the end of the current string
+    //is padded with spaces or with a specified Unicode character.
     public class StringPadRight
     {
 
