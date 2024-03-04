f15b56596617eb539de98fe68db1e728fa5c301a
Adilson <usadilsondigital@gmail.com>
2024-03-03 22:42:37 -0600

    conditional

--- StringLastIndexOfAny.cs
+++ StringLastIndexOfAny.cs
@@ -24,6 +24,14 @@ namespace ConsoleAppStrings2
             Console.WriteLine("The last character occurrence  from position {0} to 0.", start);
             Console.WriteLine("{1}{0}{2}{0}{3}{0}", Environment.NewLine, br1, br2, str);
             Console.Write("A character in '{0}' occurs at position: ", target);
+
+            at = str.LastIndexOfAny(anyOf);
+            if (at > -1)
+                Console.Write(at);
+            else
+                Console.Write("(not found)");
+            Console.Write("{0}{0}{0}", Environment.NewLine);
+
         }
     }
 }
