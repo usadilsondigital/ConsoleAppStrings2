261366420bf50919fa083ff4a5f2ff80ac24fe73
Adilson <usadilsondigital@gmail.com>
2024-03-04 23:15:42 -0600

    test4 output

--- StringReplace.cs
+++ StringReplace.cs
@@ -65,6 +65,9 @@ namespace ConsoleAppStrings2
             Console.WriteLine("The initial string: '{0}'", s);
             s = s.Replace("a", "b").Replace("b", "c").Replace("c", "d");
             Console.WriteLine("The final string: '{0}'", s);
+            // The example displays the following output:
+            //       The initial string: 'aaa'
+            //       The final string: 'ddd'
 
         }
 
