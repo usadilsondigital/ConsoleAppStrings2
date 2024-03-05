cb8997632d6d846351f15e3050be8d90b9a55bdd
Adilson <usadilsondigital@gmail.com>
2024-03-04 23:05:05 -0600

    test case

--- StringRemove.cs
+++ StringRemove.cs
@@ -9,6 +9,15 @@ namespace ConsoleAppStrings2
     //Returns a new string in which a specified number of characters from the current string are deleted.
     public class StringRemove
     {
+        public  void TestCase()
+        {
+            string s = "abc---def";
+
+            Console.WriteLine("Index: 012345678");
+            Console.WriteLine("1)     {0}", s);
+            Console.WriteLine("2)     {0}", s.Remove(3));
+            Console.WriteLine("3)     {0}", s.Remove(3, 3));
+        }
 
     }
 }
