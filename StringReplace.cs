94615408ca62eac7f90489606dcdf74b5e7d0ee8
Adilson <usadilsondigital@gmail.com>
2024-03-04 23:09:24 -0600

    class docs

--- StringReplace.cs
+++ StringReplace.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppStrings2
 {
+    //Returns a new string in which all occurrences of a specified Unicode character
+    //or String in the current string are replaced with another specified Unicode character or String.
     public class StringReplace
     {
     }
