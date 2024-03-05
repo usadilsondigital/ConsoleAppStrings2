9ced8770ed1bd46691aec2fe1eb769ceb9e8ab2d
Adilson <usadilsondigital@gmail.com>
2024-03-05 17:35:26 -0600

    test case3

--- StringSplit.cs
+++ StringSplit.cs
@@ -53,6 +53,19 @@ namespace ConsoleAppStrings2
             // Substring:
         }
 
+        public void TestCase3()
+        {
+            string s = "You win some. You lose some.";
+            char[] separators = new char[] { ' ', '.' };
+
+            string[] subs = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sub in subs)
+            {
+                Console.WriteLine($"Substring: {sub}");
+            }
+
+        }
 
 
     }
