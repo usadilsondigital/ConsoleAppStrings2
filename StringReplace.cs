3e806cf9d1ea8b6517103615873a90cc0c760189
Adilson <usadilsondigital@gmail.com>
2024-03-04 23:10:31 -0600

    test case

--- StringReplace.cs
+++ StringReplace.cs
@@ -10,5 +10,12 @@ namespace ConsoleAppStrings2
     //or String in the current string are replaced with another specified Unicode character or String.
     public class StringReplace
     {
+
+        public void TestCase() {
+            String str = "1 2 3 4 5 6 7 8 9";
+            Console.WriteLine("Original string: \"{0}\"", str);
+            Console.WriteLine("CSV string:      \"{0}\"", str.Replace(' ', ','));
+
+        }
     }
 }
