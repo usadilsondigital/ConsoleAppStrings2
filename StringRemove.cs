83f7191cccc412673e623c43217a9339e4829bda
Adilson <usadilsondigital@gmail.com>
2024-03-04 23:05:37 -0600

    test case output

--- StringRemove.cs
+++ StringRemove.cs
@@ -17,6 +17,15 @@ namespace ConsoleAppStrings2
             Console.WriteLine("1)     {0}", s);
             Console.WriteLine("2)     {0}", s.Remove(3));
             Console.WriteLine("3)     {0}", s.Remove(3, 3));
+            /*
+            This example produces the following results:
+
+            Index: 012345678
+            1)     abc---def
+            2)     abc
+            3)     abcdef
+
+            */
         }
 
     }
