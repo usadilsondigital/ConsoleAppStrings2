6db98c10549832718e8b948e64d88ac69b5a8e39
Adilson <usadilsondigital@gmail.com>
2024-03-06 22:43:31 -0600

    define target

--- StringStartWith.cs
+++ StringStartWith.cs
@@ -18,6 +18,16 @@ namespace ConsoleAppStrings2
             string msg3 = "  The string to search ends with the target string: {0}";
             bool result = false;
             CultureInfo ci;
+            // Define a target string to search for.
+            // U+00c5 = LATIN CAPITAL LETTER A WITH RING ABOVE
+                string capitalARing = "\u00c5";
+
+                // Define a string to search. 
+                // The result of combining the characters LATIN SMALL LETTER A and COMBINING 
+                // RING ABOVE (U+0061, U+030a) is linguistically equivalent to the character 
+                // LATIN SMALL LETTER A WITH RING ABOVE (U+00e5).
+                string aRingXYZ = "\u0061\u030a" + "xyz";
+
         }
     }
 }
