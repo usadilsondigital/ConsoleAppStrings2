281c98ce70926c9e4ce1252d8c8e0cdf4faa1940
Adilson <usadilsondigital@gmail.com>
2024-03-06 22:42:50 -0600

    test case

--- StringStartWith.cs
+++ StringStartWith.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleAppStrings2
 {
- 
+
     //Determines whether the beginning of this string instance matches the specified string when compared using the specified culture.
     public class StringStartWith
     {
+        public  void TestCase()
+        {
+            string msg1 = "Search for the target string \"{0}\" in the string \"{1}\".\n";
+            string msg2 = "Using the {0} - \"{1}\" culture:";
+            string msg3 = "  The string to search ends with the target string: {0}";
+            bool result = false;
+            CultureInfo ci;
+        }
     }
 }
