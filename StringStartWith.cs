dd1514fe09458cba82b2a1d8f7796a3aee03c98b
Adilson <usadilsondigital@gmail.com>
2024-03-06 22:58:49 -0600

    private docs

--- StringStartWith.cs
+++ StringStartWith.cs
@@ -140,7 +140,25 @@ namespace ConsoleAppStrings2
             }
 
             return item;
+            // The example displays the following output:
+            //    The original strings:
+            //    ---------------------
+            //    <b>This is bold text</b>
+            //    <H1>This is large Text</H1>
+            //    <b><i><font color = green>This has multiple tags</font></i></b>
+            //    <b>This has <i>embedded</i> tags.</b>
+            //    <This line simply begins with a lesser than symbol, it should not be modified
+            //
+            //    Strings after starting tags have been stripped:
+            //    -----------------------------------------------
+            //    This is bold text</b>
+            //    This is large Text</H1>
+            //    This has multiple tags</font></i></b>
+            //    This has <i>embedded</i> tags.</b>
+            //    <This line simply begins with a lesser than symbol, it should not be modified
         }
+
+
 
 
     }
