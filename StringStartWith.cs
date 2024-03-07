eff7317f8272b3f3d79e0aeb2ad6d11a645c3b47
Adilson <usadilsondigital@gmail.com>
2024-03-06 22:41:28 -0600

    start with class and docs

---
+++ StringStartWith.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+ 
+    //Determines whether the beginning of this string instance matches the specified string when compared using the specified culture.
+    public class StringStartWith
+    {
+    }
+}
