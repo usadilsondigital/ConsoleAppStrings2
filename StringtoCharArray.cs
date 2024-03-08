2d3074c74bd337f802a47300cb3058cfa7995b67
Adilson <usadilsondigital@gmail.com>
2024-03-07 23:41:41 -0600

    test case

---
+++ StringtoCharArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Copies the characters in this instance to a Unicode character array.
+    public class StringtoCharArray
+    {
+        public void TestCase()
+        {
+            string str = "012wxyz789";
+            char[] arr;
+        }
+    }
+}
