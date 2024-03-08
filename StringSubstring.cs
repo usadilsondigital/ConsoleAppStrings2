5387e80386ea2e700eebced9d6b45cbd44d60779
Adilson <usadilsondigital@gmail.com>
2024-03-07 23:16:33 -0600

    retrieve info in loop

--- StringSubstring.cs
+++ StringSubstring.cs
@@ -20,6 +20,13 @@ namespace ConsoleAppStrings2
             Console.WriteLine("The initial values in the array are:");
             foreach (string s in info)
                 Console.WriteLine(s);
+            Console.WriteLine("\nWe want to retrieve only the key information. That is:");
+            foreach (string s in info)
+            {
+                found = s.IndexOf(": ");
+                Console.WriteLine("   {0}", s.Substring(found + 2));
+            }
+
         }
 
 
