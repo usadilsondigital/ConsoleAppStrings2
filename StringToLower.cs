6cd4c146cd5e0999605863ecf910a2580b8f72b6
Adilson <usadilsondigital@gmail.com>
2024-03-07 23:45:03 -0600

    test case method

--- StringToLower.cs
+++ StringToLower.cs
@@ -9,6 +9,15 @@ namespace ConsoleAppStrings2
     //Returns a copy of this string converted to lowercase.
     public class StringToLower
     {
+        public  void TestCase()
+        {
 
+            string[] info = { "Name", "Title", "Age", "Location", "Gender" };
+
+            Console.WriteLine("The initial values in the array are:");
+            foreach (string s in info)
+                Console.WriteLine(s);
+
+        }
     }
 }
