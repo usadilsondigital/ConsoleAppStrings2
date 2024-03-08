0b1e430b678ee1319928b82e4b143833071e0bb7
Adilson <usadilsondigital@gmail.com>
2024-03-07 23:17:08 -0600

    get stubstring output

--- StringSubstring.cs
+++ StringSubstring.cs
@@ -27,6 +27,20 @@ namespace ConsoleAppStrings2
                 Console.WriteLine("   {0}", s.Substring(found + 2));
             }
 
+            // The example displays the following output:
+            //       The initial values in the array are:
+            //       Name: Felica Walker
+            //       Title: Mz.
+            //       Age: 47
+            //       Location: Paris
+            //       Gender: F
+            //       
+            //       We want to retrieve only the key information. That is:
+            //          Felica Walker
+            //          Mz.
+            //          47
+            //          Paris
+            //          F
         }
 
 
