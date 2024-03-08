599d3850d8cf4977091c01d09dd301e2f54c9559
Adilson <usadilsondigital@gmail.com>
2024-03-07 23:30:50 -0600

    try catch

--- StringSubstring.cs
+++ StringSubstring.cs
@@ -77,6 +77,15 @@ namespace ConsoleAppStrings2
             Console.WriteLine(test1);
             bool test2 = string.IsNullOrEmpty(myString.Substring(3, 0)); // This is true.
             Console.WriteLine(test2);
+            try
+            {
+                string str3 = myString.Substring(3, 1); // This throws ArgumentOutOfRangeException.
+                Console.WriteLine(str3);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
     }
