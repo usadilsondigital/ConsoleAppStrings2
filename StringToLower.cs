08d8fd6db71ba2217131b960d97b2a2c1013ec7d
Adilson <usadilsondigital@gmail.com>
2024-03-07 23:44:30 -0600

    string lower class and docs

---
+++ StringToLower.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Returns a copy of this string converted to lowercase.
+    public class StringToLower
+    {
+
+    }
+}
