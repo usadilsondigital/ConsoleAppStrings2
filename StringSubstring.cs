8fc7b572847059c5dc15190b2f4531582804c10a
Adilson <usadilsondigital@gmail.com>
2024-03-07 23:14:53 -0600

    fix typo and docs

--- StringSubstring.cs
+++ StringSubstring.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleAppStrings2
 {
-    publics class StringSubstring
+    //Retrieves a substring from this instance.This member is overloaded.For complete information about this member,
+    //including syntax, usage, and examples, click a name in the overload list.
+    public class StringSubstring
     {
     }
 }
