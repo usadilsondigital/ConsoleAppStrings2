2c09cdf11c122152333fac72bbf09c7d3f86bc6e
Adilson <usadilsondigital@gmail.com>
2024-03-08 22:13:01 -0600

    filename class exts icomparable

---
+++ FileName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    public class FileName : IComparable
+    {
+        public int CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
