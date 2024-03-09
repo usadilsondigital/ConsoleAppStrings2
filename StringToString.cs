65f906f6ffbb80492f5ac7625ac07afc68a8c11c
Adilson <usadilsondigital@gmail.com>
2024-03-08 22:33:03 -0600

    test case

---
+++ StringToString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    public class StringToString
+    {
+        public  void TestCase()
+        {
+            String str1 = "123";
+            String str2 = "abc";
+
+            Console.WriteLine("Original str1: {0}", str1);
+            Console.WriteLine("Original str2: {0}", str2);
+            Console.WriteLine("str1 same as str2?: {0}", Object.ReferenceEquals(str1, str2));
+
+           
+        }
+    }
+}
