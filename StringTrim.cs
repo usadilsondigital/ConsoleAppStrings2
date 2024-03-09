5efc0e657221206e3d22ae1eb94bb978b6bbb874
Adilson <usadilsondigital@gmail.com>
2024-03-08 23:02:28 -0600

    console name

--- StringTrim.cs
+++ StringTrim.cs
@@ -15,6 +15,19 @@ namespace ConsoleAppStrings2
         {
             Console.Write("Enter your first name: ");
             string firstName = Console.ReadLine();
+            Console.Write("Enter your middle name or initial: ");
+            string middleName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("You entered '{0}', '{1}', and '{2}'.",
+                            firstName, middleName, lastName);
+
+            string name = ((firstName.Trim() + " " + middleName.Trim()).Trim() + " " +
+                        lastName.Trim()).Trim();
+            Console.WriteLine("The result is " + name + ".");
+
         }
     }
  }
