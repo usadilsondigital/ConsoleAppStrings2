b37a06e6708e5f348d1ad655341a9fce3e2be6cc
Adilson <usadilsondigital@gmail.com>
2024-03-08 22:10:53 -0600

    string best practices class and docs

---
+++ StringBestPractices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    public class StringBestPractices
+    {
+        /*StringComparison member Description
+        CurrentCulture  Performs a case-sensitive comparison using the current culture.
+        CurrentCultureIgnoreCase    Performs a case-insensitive comparison using the current culture.
+        InvariantCulture    Performs a case-sensitive comparison using the invariant culture.
+        InvariantCultureIgnoreCase  Performs a case-insensitive comparison using the invariant culture.
+        Ordinal Performs an ordinal comparison.
+        OrdinalIgnoreCase Performs a case-insensitive ordinal comparison.*/
+    }
+}
