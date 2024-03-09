eb9e85e57d1a55b5790bee7444ea134eff0deb5c
Adilson <usadilsondigital@gmail.com>
2024-03-08 22:37:11 -0600

    toupper method and loop

--- StringToUpper.cs
+++ StringToUpper.cs
@@ -9,6 +9,16 @@ namespace ConsoleAppStrings2
     //Returns a copy of this string converted to uppercase.
     public class StringToUpper
     {
+        public  void ToUpper()
+        {
+            int n = 0;
+            for (int ctr = 0x20; ctr <= 0x017F; ctr++)
+            {
+                string string1 = ((char)ctr).ToString();
+                string upperString = string1.ToUpper();
+                
+            }
+        }
 
     }
 }
