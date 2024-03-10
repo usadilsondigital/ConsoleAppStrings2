3e62bc6a1aef2b4be16de9a49d3fdb54946aa506
Adilson <usadilsondigital@gmail.com>
2024-03-09 20:44:59 -0600

    trim end class and docs

---
+++ StringTrimEnd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Removes all the trailing white-space characters from the current string.
+    public class StringTrimEnd
+    {
+
+    }
+}
