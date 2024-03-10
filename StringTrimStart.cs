eca2e8db0a958f876ce3fdb24abc3c13655b71a6
Adilson <usadilsondigital@gmail.com>
2024-03-09 20:57:14 -0600

    code output

--- StringTrimStart.cs
+++ StringTrimStart.cs
@@ -88,6 +88,24 @@ namespace ConsoleAppStrings2
                 Console.WriteLine("   {0}", line);
 
         }
+        // This code produces the following output to the console:
+        //    Before call to StripComments:
+        //       using System;
+        //   
+        //       public class HelloWorld
+        //       {
+        //           public static void Main()
+        //           {
+        //               // This code displays a simple greeting
+        //               // to the console.
+        //               Console.WriteLine("Hello, World.");
+        //           }
+        //       }  
+        //    After call to StripComments:
+        //       This code displays a simple greeting
+        //       to the console.
 
-        }
+
+
+    }
 }
