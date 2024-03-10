9410178c02b59f6f392c427a43d16d3d94821012
Adilson <usadilsondigital@gmail.com>
2024-03-09 20:46:56 -0600

    docs

--- StringTrimEnd.cs
+++ StringTrimEnd.cs
@@ -9,6 +9,25 @@ namespace ConsoleAppStrings2
     //Removes all the trailing white-space characters from the current string.
     public class StringTrimEnd
     {
+        public void TestCase()
+        {
+            string sentence = "The dog had a bone, a ball, and other toys.";
+            char[] charsToTrim = { ',', '.', ' ' };
+            string[] words = sentence.Split();
+            foreach (string word in words)
+                Console.WriteLine(word.TrimEnd(charsToTrim));
 
+            // The example displays the following output:
+            //       The
+            //       dog
+            //       had
+            //       a
+            //       bone
+            //       a
+            //       ball
+            //       and
+            //       other
+            //       toys
+        }
     }
 }
