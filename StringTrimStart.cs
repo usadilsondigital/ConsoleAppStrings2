ad3e89b0017d78495f03cdb4b698db41c362d681
Adilson <usadilsondigital@gmail.com>
2024-03-09 20:54:55 -0600

    strip comments method

--- StringTrimStart.cs
+++ StringTrimStart.cs
@@ -53,5 +53,16 @@ namespace ConsoleAppStrings2
 
         }
 
+        public  string[] StripComments(string[] lines)
+        {
+            List<string> lineList = new List<string>();
+            foreach (string line in lines)
+            {
+                if (line.TrimStart(' ').StartsWith("//"))
+                    lineList.Add(line.TrimStart(' ', '/'));
+            }
+            return lineList.ToArray();
+        }
+
     }
 }
