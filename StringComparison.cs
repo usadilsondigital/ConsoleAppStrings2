6ab53b2653221b8b41f852524d734d09d78d7c03
Adilson <usadilsondigital@gmail.com>
2024-03-11 23:22:00 -0500

    double loop

--- StringComparison.cs
+++ StringComparison.cs
@@ -22,6 +22,17 @@ namespace ConsoleAppStrings2
                 Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureName);
                 Console.WriteLine("Current Culture: {0}", CultureInfo.CurrentCulture.Name);
 
+                for (int ctr = 0; ctr <= strings1.GetUpperBound(0); ctr++)
+                {
+                    foreach (var comparison in comparisons)
+                        Console.WriteLine("   {0} = {1} ({2}): {3}", strings1[ctr],
+                                          strings2[ctr], comparison,
+                                          String.Equals(strings1[ctr], strings2[ctr], comparison));
+
+                    Console.WriteLine();
+                }
+
+
             }
 
         }
