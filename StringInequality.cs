847baa246f10570f18bb5970a1efbc59d4b1008f
Adilson <usadilsondigital@gmail.com>
2024-03-11 23:15:59 -0500

    test case

--- StringInequality.cs
+++ StringInequality.cs
@@ -9,5 +9,18 @@ namespace ConsoleAppStrings2
     //Determines whether two specified strings have different values.
     public class StringInequality
     {
+
+        public  void TestCase()
+        {
+            Console.WriteLine(
+                "This example of the String Inequality operator\n" +
+                "generates the following output.\n");
+
+            CompareAndDisplay("ijkl");
+            CompareAndDisplay("ABCD");
+            CompareAndDisplay("abcd");
+        }
+
+
     }
 }
