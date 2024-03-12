58d41cfd57ccac846fc7300937d913bf7264eea0
Adilson <usadilsondigital@gmail.com>
2024-03-11 23:12:06 -0500

    test case

--- StringEquality.cs
+++ StringEquality.cs
@@ -9,5 +9,17 @@ namespace ConsoleAppStrings2
     //Determines whether two specified strings have the same value.
     public class StringEquality
     {
+
+        public  void TestCase()
+        {
+            Console.WriteLine("This example of the String Equality operator\n" +
+                "generates the following output.\n");
+
+            CompareAndDisplay("ijkl");
+            CompareAndDisplay("ABCD");
+            CompareAndDisplay("abcd");
+        }
+
+
     }
 }
