a74e65ba0f4f8ddd762a4e35fdfa0ae25b92c31a
Adilson <usadilsondigital@gmail.com>
2024-03-11 23:19:18 -0500

    string comparison

---
+++ StringComparison.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStrings2
+{
+    //Specifies the culture, case, and sort rules to be used by certain
+    //overloads of the Compare(String, String) and Equals(Object) methods
+    public class StringComparison
+    {
+    }
+}
