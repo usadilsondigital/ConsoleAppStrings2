f3e06a563ef4c6a2dd347c59ae865d6b2f2e3a50
Adilson <usadilsondigital@gmail.com>
2024-03-12 22:18:57 -0500

    deconstruct

--- StructTimeOnly.cs
+++ StructTimeOnly.cs
@@ -45,7 +45,7 @@ namespace ConsoleAppStrings2
 
         public int CompareTo(object? value);
 
-
+        public void Deconstruct(out int hour, out int minute, out int second, out int millisecond, out int microsecond);
 
     }
 }
