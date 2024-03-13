3f860bd25be2d26d8121237ed08cf25f53a305fd
Adilson <usadilsondigital@gmail.com>
2024-03-12 22:14:15 -0500

    docs

--- StructTimeOnly.cs
+++ StructTimeOnly.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppStrings2
 {
-    internal class StructTimeOnly
+    //Represents a time of day, as would be read from a clock, within the range 00:00:00 to 23:59:59.9999999.
+    public class StructTimeOnly
     {
     }
 }
