a228ed98f7323bb6fcec0fed5d18b5a43a9feb45
Adilson <usadilsondigital@gmail.com>
2024-03-12 22:15:17 -0500

    properties docs

--- StructTimeOnly.cs
+++ StructTimeOnly.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,30 @@ namespace ConsoleAppStrings2
     //Represents a time of day, as would be read from a clock, within the range 00:00:00 to 23:59:59.9999999.
     public class StructTimeOnly
     {
+        //properties
+      /*  Hour
+        Gets the hour component of the time represented by this instance.
+
+        MaxValue
+        Gets the largest possible value of TimeOnly.
+
+        Microsecond
+        Gets the microsecond component of the time represented by this instance.
+                Millisecond
+        Gets the millisecond component of the time represented by this instance.
+                Minute
+        Gets the minute component of the time represented by this instance.
+
+        MinValue
+        Gets the smallest possible value of TimeOnly.
+
+        Nanosecond
+        Gets the nanosecond component of the time represented by this instance.
+                Second
+        Gets the seconds component of the time represented by this instance.
+
+        Ticks
+        Gets the number of ticks that represent the time of this instance.
+              */
     }
 }
